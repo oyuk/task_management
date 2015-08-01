fdbecba749cfac0782850a782cd4bc036b4dc95c
oky <reidreid68@gmail.com>
2015-08-01 23:39:02 +0900

    [from now] 2015/08/01 23:39:02
     app/controllers/tasks_controller.rb |  2 ++
     app/views/groups/index.html.erb     |  2 +-
     app/views/groups/show.html.erb      | 25 ++++++++++++++++++++++---
     app/views/tasks/show.html.erb       |  0
     config/routes.rb                    |  1 +
     5 files changed, 26 insertions(+), 4 deletions(-)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,14 @@
 class TasksController < Base
 
+  def show
+    @task = Task.find(params[:id])
+  end
 
+  def new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   mount Api::API => '/api'
 
   resources :groups
+  resources :tasks
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
